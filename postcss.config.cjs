9694ef08f667c13b70f2ab7432d44ee6ca5a8629
tanveer-98 <tnvrahmed98@gmail.com>
2022-11-21 21:01:31 +0530

    added tailwind postconfig missing contents

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,5 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+const tailwindcss = require('tailwindcss');
+
+module.exports  = {
+    plugins : [tailwindcss('./tailwind.config.cjs'),require('autoprefixer')]
 }
